9bf4220efb944ff85a6e712f9e100c8f5405b341
Safiamoon <msellek.safia@gmail.com>
2021-02-23 19:26:53 +0100

    landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,9 +10,6 @@ function Home() {
         <div className="row p-0" style={{backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)),url('${firstImage}')`, backgroundRepeat:"no-repeat", minHeight:"92vh"}}>
             <div className="col-md-12 d-flex flex-column justify-content-center align-items-center text-white font-weight-bold">
                 <h1 className="d-none d-lg-inline-flex ">MAKE YOUR LIFE EASIER !</h1>
-                    <div className="btn-group d-inline mt-5">
-                        {/* <Link className="nav-link  btn btn-outline btn-lg mb1 silver font-weight-bold d-inline mx-4 text-info" to={CONSTANTS.APPARTEMENTS}>DISCOVER MORE</Link> */}
-                    </div>
             </div>
         </div>
         </>
